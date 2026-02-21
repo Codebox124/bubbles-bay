514ba6e87fd46b9626aac29d4323d1059ac7f293
Enxgma-thecodecrusader <158729288+Enxgma-thecodecrusader@users.noreply.github.com>
2026-02-21 16:51:19 +0100

    logo change

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Logo = () => {
-    return (
-        <div>
-            <Image src="/bubbles.png" alt="Logo" width={90} height={90} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Image src="/hero.jpg" alt="Logo" width={90} height={90} />
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
